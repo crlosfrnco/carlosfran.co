6453b916bb853cdae54e8da8d4db627286651f78
Carlos Franco <56747322+crlosfrnco@users.noreply.github.com>
2025-04-22 22:52:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-$(document).ready(function() {
-  $(document).on('mousemove', function(e) {
-    $('#circularcursor').css({
-      left: e.pageX,
-      top: e.pageY
-    });
-  })
-});
+document.body.onmousemove = function(e) {
+  document.documentElement.style.setProperty (
+    '--x', (
+      e.clientX+window.scrollX
+    )
+    + 'px'
+  );
+  document.documentElement.style.setProperty (
+    '--y', (
+      e.clientY+window.scrollY
+    ) 
+    + 'px'
+  );
+}
